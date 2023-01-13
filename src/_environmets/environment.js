1d872e91759342730027601c591375d5358305c5
federico <federicosicoli@gmail.com>
2023-01-13 13:22:19 +0100

    prueba preview 2

--- src/_environmets/environment.js
+++ src/_environmets/environment.js
@@ -1,8 +1,7 @@
 export const environment = {
    production: false,
-
    // api
    //BASE_API_URL: "http://localhost:3000",
-   BASE_API_URL:"ckmovies-production.up.railway.app",
+   BASE_API_URL:"https://backmovies-production.up.railway.app",
    IMAGES_URL: "https://image.tmdb.org/t/p",
 };
